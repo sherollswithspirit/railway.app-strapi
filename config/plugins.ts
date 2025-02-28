25335d7263bbac4b795b740456b5a40064ec6419
βok Rivera <bok.rivera@simplicitygroup.com>
2025-02-27 23:12:05 -0500

    plugin update email config

--- config/plugins.ts
+++ config/plugins.ts
@@ -19,16 +19,21 @@ export default ({ env }) => ({
             },
         },
     },
-    // email: {
-    //     config: {
-    //         provider: 'postmark',
-    //         providerOptions: {
-    //             apiKey: env('POSTMARK_API_KEY'),
-    //         },
-    //         settings: {
-    //             defaultFrom: env('POSTMARK_DEFAULT_FROM'),
-    //             defaultReplyTo: env('POSTMARK_DEFAULT_REPLY_TO'),
-    //         },
-    //     },
-    // },
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                host: env('SMTP_HOST', 'smtp.example.com'),
+                port: env('SMTP_PORT', 587),
+                auth: {
+                    user: env('SMTP_USERNAME'),
+                    pass: env('SMTP_PASSWORD'),
+                },
+            },
+            settings: {
+                defaultFrom: 'sherollswithspirit@gmail.com',
+                defaultReplyTo: 'sherollswithspirit@gmail.com',
+            },
+        },
+    },
 });
